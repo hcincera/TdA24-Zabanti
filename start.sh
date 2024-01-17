2a2da92a76ee1e8b4f00eab5173293a7feb10951
Jan Cincera <hcincera@gmail.com>
2024-01-17 18:02:32 +0100

    use different port for dev testing

--- start.sh
+++ start.sh
@@ -6,10 +6,16 @@ if [ "$1" == "docker" ]; then
 	exit
 fi
 
-if [ -f /.dockerenv ]; then
-    export PORT=80
+if [ "$1" == "dev" ]; then
+	docker build . -t tda-flask
+	docker run -it -p 8069:80 -v ${PWD}:/app tda-flask
+	exit
 else
-    export PORT=8069
+	if [ -f /.dockerenv ]; then
+		export PORT=80
+	else
+		export PORT=8069
+	fi
 fi
 
 python3 -m flask --app app/app.py init-db
