b202d558db886dee7ba7d132239bc1d7e6ae214a
Jan Cincera <hcincera@gmail.com>
2024-01-17 18:10:29 +0100

    fix the fix that fixed nothing

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-if [ "$1" == "docker" ]; then
+if [ "$1" = "docker" ] ; then
 	docker build . -t tda-flask
 	docker run -it -p 8080:80 -v ${PWD}:/app tda-flask
 	exit
 fi
 
-if [ "$1" == "dev" ]; then
+if [ "$1" = "dev" ] ; then
 	docker build . -t tda-flask
 	docker run -it -p 8069:80 -v ${PWD}:/app tda-flask
 	exit
 fi
 
-if [ -f /.dockerenv ]; then
+if [ -f /.dockerenv ] ; then
 	export PORT=80
 else
 	export PORT=8069
